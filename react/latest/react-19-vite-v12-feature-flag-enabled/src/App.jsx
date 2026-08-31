7b0d8772c808225c223c480362513e4946d65323
Anna Wen <54281166+annawen1@users.noreply.github.com>
2026-08-31 09:03:43 -0400

    chore: set up clickable tile

--- react/latest/react-19-vite-v12-feature-flag-enabled/src/App.jsx
+++ react/latest/react-19-vite-v12-feature-flag-enabled/src/App.jsx
@@ -1,9 +1,13 @@
-import { Button } from "@carbon/react";
+import { ClickableTile } from "@carbon/react";
 
 export default function App() {
   return (
     <div>
-      <Button>Hello world</Button>
+      <ClickableTile
+        id="clickable-tile"
+        href="https://www.carbondesignsystem.com/">
+        Clickable Tile with v12 default icon enabled
+      </ClickableTile>
     </div>
   );
 }
